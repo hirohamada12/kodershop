9da9583bb878776b276c5b3329db0ec2ebd766ba
hirohamada12 <36723610+hirohamada12@users.noreply.github.com>
2019-10-06 21:56:14 +0700

    AnhVTN11 Navbar menu, translation,

--- KoderGang_Web/src/containers/App/index.js
+++ KoderGang_Web/src/containers/App/index.js
@@ -2,20 +2,24 @@ import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 
 import {Home, NotFound} from '../'
+import Header from "../../layout/header/Header";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="container mt-4">
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route component={NotFound}/>
-          </Switch>
-        </div>
-      </div>
-    )
-  }
+
+    render() {
+
+        return (
+            <div className="app">
+                <Header/>
+                <div className="container mt-4">
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App
--- KoderGang_Web/src/index.js
+++ KoderGang_Web/src/index.js
@@ -1,34 +1,51 @@
 import registerServiceWorker from './registerServiceWorker'
-import React from 'react'
+import React, {Suspense} from 'react'
 import ReactDOM from 'react-dom'
+
 import {BrowserRouter} from 'react-router-dom'
-import {createStore, applyMiddleware} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import {createLogger} from 'redux-logger'
 import reducer from './reducers'
 import App from './containers/App'
 
+
+import "@babel/polyfill";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import './styles.css'
+import Loader from "react-loader-spinner";
 
 const loggerMiddleware = createLogger();
 
 const store = createStore(
-  reducer,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
-  )
+    reducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
 );
 
+const loading = () => (
+    <div id="preloader">
+        <Loader
+            type="Puff"
+            color="#04d39f"
+            height={100}
+            width={100}
+        />
+    </div>
+);
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+    <Suspense fallback={loading()}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </Suspense>,
+    document.getElementById('root')
 );
 
 registerServiceWorker();
